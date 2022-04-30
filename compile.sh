4996ebf25bcc0d6abc91fe012a5a56d62ed62541
William Faircloth <willuhmjs@gmail.com>
2022-04-30 16:04:09 -0400

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,5 +1,7 @@
+#!/bin/sh
+
 cd src
-deno compile -o ../target/x86_64-unknown-linux-gnu --target x86_64-unknown-linux-gnu index.ts
-deno compile -o ../target/x86_64-pc-windows-msvc --target x86_64-pc-windows-msvc index.ts
-deno compile -o ../target/x86_64-apple-darwin --target x86_64-apple-darwin index.ts
-deno compile -o ../target/aarch64-apple-darwin --target aarch64-apple-darwin index.ts
+deno compile --allow-read --allow-write -o ../target/x86_64-unknown-linux-gnu --target x86_64-unknown-linux-gnu index.ts
+deno compile --allow-read --allow-write -o ../target/x86_64-pc-windows-msvc --target x86_64-pc-windows-msvc index.ts
+deno compile --allow-read --allow-write -o ../target/x86_64-apple-darwin --target x86_64-apple-darwin index.ts
+deno compile --allow-read --allow-write -o ../target/aarch64-apple-darwin --target aarch64-apple-darwin index.ts
