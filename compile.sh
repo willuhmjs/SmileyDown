87dcf8174290648a4cc07b695c749ab228a58cc1
William Faircloth <willuhmjs@gmail.com>
2022-04-30 15:54:43 -0400

    add read and write perms?

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 cd src
-deno compile -o ../target/x86_64-unknown-linux-gnu --target x86_64-unknown-linux-gnu index.ts
-deno compile -o ../target/x86_64-pc-windows-msvc --target x86_64-pc-windows-msvc index.ts
-deno compile -o ../target/x86_64-apple-darwin --target x86_64-apple-darwin index.ts
-deno compile -o ../target/aarch64-apple-darwin --target aarch64-apple-darwin index.ts
+deno compile -o ../target/x86_64-unknown-linux-gnu --target x86_64-unknown-linux-gnu index.ts --allow-read --allow-write
+deno compile -o ../target/x86_64-pc-windows-msvc --target x86_64-pc-windows-msvc index.ts --allow-read --allow-write
+deno compile -o ../target/x86_64-apple-darwin --target x86_64-apple-darwin index.ts --allow-read --allow-write
+deno compile -o ../target/aarch64-apple-darwin --target aarch64-apple-darwin index.ts --allow-read --allow-write
